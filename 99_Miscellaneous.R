daa5d84574ef95d6c1c1a0d8a48f87279c4ab0d2
Quang C. Huynh <quang@bluematterscience.com>
2021-12-09 14:54:32 -0800

    Combine cod/pollock plot of SSB/rec estimates

--- 99_Miscellaneous.R
+++ 99_Miscellaneous.R
@@ -104,3 +104,82 @@ ggplot(indicators_cast %>% filter(MP == "M02_ra" & Year == 2030), aes(PMat_1_mu,
   gfplot::theme_pbs() + no_panel_gap + legend_bottom
 ggsave("report/ppt/LDA_singlepanel.png", height = 3, width = 3)
 
+
+
+###### Combine comparison plot with both cod and pollock
+no_panel_gap <- theme(panel.spacing = unit(0, "in"))
+legend_bottom <- theme(legend.position = "bottom")
+
+
+cod <- local({
+  
+  res1 <- readRDS("GoM_cod/SRA_cod_M02.rds")
+  res2 <- readRDS("GoM_cod/SRA_cod_MRAMP.rds")
+  res3 <- readRDS("GoM_cod/SRA_NR1.rds")
+  res4 <- readRDS("GoM_cod/SRA_NR2.rds")
+  res5 <- readRDS("GoM_cod/SRA_NR3.rds")
+  
+  Model <- data.frame(name = c("MC", "IM", "MCIM", "M02", "MRAMP"), type = c("OM", "OM", "OM", "AM", "AM"))
+  SSB <- lapply(list(res3, res4, res5, res1, res2), function(x) x@SSB[1, ]) %>% do.call(cbind, .) %>% 
+    structure(dimnames = list(Year = 1982:2019, name = Model$name)) %>% reshape2::melt() %>% mutate(y = "SSB") %>%
+    left_join(Model, by = "name")
+  
+  RR <- lapply(list(res3, res4, res5, res1, res2), function(x) x@Misc[[1]]$R) %>% do.call(cbind, .) %>% 
+    structure(dimnames = list(Year = 1982:2019, name = Model$name)) %>% reshape2::melt() %>% mutate(y = "Recruitment") %>%
+    left_join(Model, by = "name")
+  
+  rbind(SSB, RR) %>% 
+    mutate(y = factor(y, levels = c("SSB", "Recruitment")), 
+           name = factor(name, levels = Model$name), Stock = "Cod") %>% 
+    filter(value <= 1e6) %>%
+    ggplot(aes(x = Year, y = value, linetype = name, colour = name)) + geom_line() +
+    geom_hline(yintercept = 0, colour = "white") +
+    facet_grid(y ~ Stock, scales = "free_y", switch = "y") + ylab(NULL) + 
+    geom_text(data = data.frame(x = Inf, yy = Inf, y = c("SSB", "Recruitment"), txt = c("(a)", "(b)")),
+              aes(x, yy, label = txt), hjust = 1.1, vjust = 1.1,
+              inherit.aes = FALSE) + 
+    scale_y_continuous(labels = scales::comma, n.breaks = 5) +
+    scale_linetype_manual(values = c("MC" = 1, "IM" = 1, "MCIM" = 1, "M02" = 2, "MRAMP" = 2)) +
+    gfplot::theme_pbs() + no_panel_gap + legend_bottom + 
+    theme(legend.title = element_blank(), strip.placement = "outside")
+})
+
+
+pollock <- local({
+  
+  res1 <- readRDS("pollock/SRA_pollock_base.rds")
+  res2 <- readRDS("pollock/SRA_pollock_flatsel.rds")
+  res3 <- readRDS("pollock/SRA_NR1.rds")
+  res4 <- readRDS("pollock/SRA_NR2.rds")
+  res5 <- readRDS("pollock/SRA_NR3.rds")
+  
+  # SSB and R
+  Model <- data.frame(name = c("SS", "SWB", "SWF", "Base", "FlatSel"), type = c("OM", "OM", "OM", "AM", "AM"))
+  SSB <- lapply(list(res3, res4, res5, res1, res2), function(x) x@SSB[1, ]) %>% do.call(cbind, .) %>% 
+    structure(dimnames = list(Year = 1970:2019, name = Model$name)) %>% reshape2::melt() %>% mutate(y = "SSB") %>%
+    left_join(Model, by = "name")
+  
+  RR <- lapply(list(res3, res4, res5, res1, res2), function(x) x@Misc[[1]]$R) %>% do.call(cbind, .) %>% 
+    structure(dimnames = list(Year = 1970:2019, name = Model$name)) %>% reshape2::melt() %>% mutate(y = "Recruitment") %>%
+    left_join(Model, by = "name")
+  
+  rbind(SSB, RR) %>% 
+    mutate(y = factor(y, levels = c("SSB", "Recruitment")), 
+           name = factor(name, levels = Model$name), Stock = "Pollock") %>% 
+    filter(value <= 1e6) %>%
+    ggplot(aes(x = Year, y = value, linetype = name, colour = name)) + geom_line() +
+    geom_hline(yintercept = 0, colour = "white") +
+    geom_text(data = data.frame(x = Inf, yy = Inf, y = c("SSB", "Recruitment"), txt = c("(c)", "(d)")),
+              aes(x, yy, label = txt), hjust = 1.1, vjust = 1.1,
+              inherit.aes = FALSE) + 
+    facet_grid(y ~ Stock, scales = "free_y", switch = "y") + ylab(NULL) +
+    scale_y_continuous(labels = scales::comma, n.breaks = 5) +
+    scale_linetype_manual(values = c("SS" = 1, "SWB" = 1, "SWF" = 1, "Base" = 2, "FlatSel" = 2)) +
+    gfplot::theme_pbs() + no_panel_gap + legend_bottom + 
+    theme(legend.title = element_blank(), strip.placement = "outside")
+})
+
+
+ggpubr::ggarrange(cod, pollock, ncol = 2)
+ggsave("report/model_compare.png", height = 4, width = 8)
+
